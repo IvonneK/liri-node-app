175212e046937c137c89bea98314251018ff9f62
Ivonne Komis <ivonnekomis@Ivonnes-MBP.fios-router.home>
2017-01-23 09:27:00 -0500

    init commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this key.js is loaded');
+
+exports.twitterKey = {
+	consumer_key: '<input here>',
+	consumer_secret: '<input here>',
+	access_token_key: '<input here>',
+	access_token_secret: '<input here>'
+}
---
+++ liri.js
@@ -0,0 +1,9 @@
+require('keys.js')
+console.log('')
+// "my-tweets"
+// "spotify-this-song"
+// "move-this"
+// "do-what-it-says"
+
+// node.liri.js my-tweets
+// this should show your last 20 tweets
