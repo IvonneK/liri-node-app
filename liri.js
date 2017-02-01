81cb1518707c2bb15109575e651e824165d9c248
Ivonne Komis <ivonnekomis@Ivonnes-MBP.fios-router.home>
2017-02-01 16:49:33 -0500

    update with ifs

--- liri.js
+++ liri.js
@@ -7,22 +7,52 @@
 
 var request = require('request');
 var twitter = require('twitter');
-var fs = require('fs');
+var spotify = require('spotify');
 var keys = require('./keys.js');
 
 var keyList = keys.twitterKey;
 for (var key in keyList) {
 	console.log(key + ': ' + keyList[key]);
 }
+var requestEntered = process.argv;
+var userInput = requestEntered[2];
 
+var ikomisTweets = [];
 
-// console.log('')
-// // "my-tweets"
-// // "spotify-this-song"
-// // "move-this"
-// // "do-what-it-says"
-
+// my-tweets
+// spotify-this-song
+// "move-this"
+// "do-what-it-says"
 // // node.liri.js my-tweets
 // // this should show your last 20 tweets
+
+if (userInput === "my-tweets") {
+  console.log('my tweets:' + ikomisTweets) 
+}
+
+else if (userInput === "spotify-this-song") {
+  console.log('my tweets:' + ikomisTweets) 
+}
+
+else if (userInput === "move-this") {
+  console.log('my tweets:' + ikomisTweets) 
+}
+
+else if (userInput === "do-what-it-says") {
+  console.log('my tweets:' + ikomisTweets) 
+}
+
+else {
+  console.log("Not a recognized command");
+}
+
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    // Do something with 'data' 
+});
 
 
