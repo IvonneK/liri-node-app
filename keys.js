ddae31f479ee64af372adef416eb19b477c648de
Ivonne Komis <ivonnekomis@Ivonnes-MBP.fios-router.home>
2017-02-01 14:47:32 -0500

    able to read keys

--- keys.js
+++ keys.js
@@ -1,8 +1,6 @@
-console.log('this key.js is loaded');
-
 exports.twitterKey = {
-	consumer_key: '<input here>',
-	consumer_secret: '<input here>',
-	access_token_key: '<input here>',
-	access_token_secret: '<input here>'
-}
+	consumer_key: 'umxp5n8cDN10fmOjp7USasoSd',
+	consumer_secret: 'LUTkDzZRl6n38v2pNKx0CmKbmm4LO4c51Ra9Zi8cCvQpnaomQp',
+	access_token_key: '950087166-XkNkJk0bKoPe5CsFLkmJRdENeJr0ybadaCEFPYsI',
+	access_token_secret: 'GzRsoENkderraosTkvn0JzSdrYZwqpF9HIkJZlXan1HxO'
+};
--- liri.js
+++ liri.js
@@ -1,9 +1,28 @@
-require('keys.js')
-console.log('')
-// "my-tweets"
-// "spotify-this-song"
-// "move-this"
-// "do-what-it-says"
+// Coder: Ivonne Komis
+// Purpose: use LIRI language interpretation and recognition to send 
+// requests
+// API used: twitter, spotify and IMDB
+// use api keys for twitter from keys.js 
+// tested able to read keys (./ added)
 
-// node.liri.js my-tweets
-// this should show your last 20 tweets
+var request = require('request');
+var twitter = require('twitter');
+var fs = require('fs');
+var keys = require('./keys.js');
+
+var keyList = keys.twitterKey;
+for (var key in keyList) {
+	console.log(key + ': ' + keyList[key]);
+}
+
+
+// console.log('')
+// // "my-tweets"
+// // "spotify-this-song"
+// // "move-this"
+// // "do-what-it-says"
+
+// // node.liri.js my-tweets
+// // this should show your last 20 tweets
+
+
